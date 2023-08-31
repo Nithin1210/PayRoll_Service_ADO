3e19213dd1a98fb888e3ffa352245277349bd504
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-31 16:16:58 +0530

    [Nithin] Add : Updation of Salary Based on Gender

--- PayRollService_ADO/Program.cs
+++ PayRollService_ADO/Program.cs
@@ -8,31 +8,32 @@ namespace PayRollService_ADO
         static void Main()
         {
             //Payroll employee Stored Proceudre
-            PayrollEmployee employee = new PayrollEmployee()
-            {
-                id = 6,
-                name = "Teressa",
-                salary = "20000",
-                start_date = "2018/01/01",
-                gender = 'F',
-                phone = "9898789898",
-                address = "chennai",
-                department = "Marketing",
-                basic_pay = 300,
-                deductions = 300,
-                taxable_pay = 30,
-                income_tax = 300,
-                net_pay = 300,
-            };
+            //PayrollEmployee employee = new PayrollEmployee()
+            //{
+            //    id = 6,
+            //    name = "Teressa",
+            //    salary = "20000",
+            //    start_date = "2018/01/01",
+            //    gender = 'F',
+            //    phone = "9898789898",
+            //    address = "chennai",
+            //    department = "Marketing",
+            //    basic_pay = 300,
+            //    deductions = 300,
+            //    taxable_pay = 30,
+            //    income_tax = 300,
+            //    net_pay = 300,
+            //};
 
 
             Payroll_Operation payroll = new Payroll_Operation();
-            payroll.CreateTable();
-            payroll.UpdateEmployee(employee);
-            payroll.RetreivedatainaParticularPeriod("2019-01-01");
-            payroll.GetAllEmployeeDetails();
+            //payroll.CreateTable();
+            //payroll.UpdateEmployee(employee);
+            //payroll.RetreivedatainaParticularPeriod("2019-01-01");
+            //payroll.GetAllEmployeeDetails();
+            payroll.SumAvgMinMax();
 
-            
+
         }
 
     }

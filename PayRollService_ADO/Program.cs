e946a3bf77e18df0727a19367ba1bf7e9af2fd19
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-31 15:56:00 +0530

    [Nithin]Add : Updation of updating the value in new line

--- PayRollService_ADO/Program.cs
+++ PayRollService_ADO/Program.cs
@@ -8,11 +8,31 @@ namespace PayRollService_ADO
         static void Main()
         {
             //Payroll employee Stored Proceudre
+            PayrollEmployee employee = new PayrollEmployee()
+            {
+                id = 6,
+                name = "Teressa",
+                salary = "2000",
+                start_date = "2018/01/01",
+                gender = 'F',
+                phone = "9898789898",
+                address = "chennai",
+                department = "Marketing",
+                basic_pay = 300,
+                deductions = 300,
+                taxable_pay = 30,
+                income_tax = 300,
+                net_pay = 300,
+            };
+
 
             Payroll_Operation payroll = new Payroll_Operation();
             payroll.CreateTable();
+            payroll.UpdateEmployee(employee);
             payroll.GetAllEmployeeDetails();
+
+            
         }
 
     }
-}   
+}
